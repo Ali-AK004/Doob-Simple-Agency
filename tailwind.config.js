20495c830a93e913a7df31e52d8a814d0f335a20
AliMohamed <aak842653@gmail.com>
2024-04-28 23:28:09 +0300

    changed The Project's Structure

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/*.{html,js}"],
+  content: ['./*.{html,js}'],
   theme: {
     extend: {},
   },
-  plugins: ["prettier-plugin-tailwindcss"],
+  plugins: ['prettier-plugin-tailwindcss'],
 };
